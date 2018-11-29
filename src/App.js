a5a0931043cc84bc89dc0e2f662ce08f367d69b6
Nick Pierson <me@nick.exposed>
2018-11-29 02:24:09 +0000

    center canvas, and draw background

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React, {Component} from 'react';
 import {play} from './mocha/player';
 
 class App extends Component {
-  onDisconnectClicked = () => {
-    this.game.stop();
-  };
+  constructor() {
+    super();
+
+    this.state = {size: 512};
+  }
 
   onCanvasRef = ref => {
     play(ref).then(game => (this.game = game));
   };
 
   render() {
+    const {size} = this.state;
+
     return (
       <div className="app">
         <canvas
-          height="512"
+          height={size}
           id="mocha"
           ref={this.onCanvasRef}
           tabIndex="1"
-          width="512"
+          width={size}
         />
-        <button onClick={this.onDisconnectClicked}>Disconnect</button>
       </div>
     );
   }
